b9c6fdd1d70d3fbf47db1937b023bba6d30e6936
arnaldoselles <arnaldo.p.selles@gmail.com>
2026-09-09 11:14:05 -0500

    Corregida una alerta en el metodo mayor que 192 y detalles visuales(orientacion del texto de la letra del himno y los botones de A+ y A- )

--- MauiCurso/MayorQue192Converter.cs
+++ MauiCurso/MayorQue192Converter.cs
@@ -9,7 +9,7 @@ namespace MauiCurso
 {
     public class MayorQue192Converter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             if (value is int numero)
             {
@@ -21,7 +21,7 @@ namespace MauiCurso
             return false;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
